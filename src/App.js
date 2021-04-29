7c71d66d16501c1c4979c65c10cfb8682470e763
Heze <64218603+Hcmwebs@users.noreply.github.com>
2021-04-30 11:01:07 +1200

    styles auto updated from sass --watch

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Header from './js/Header';
 import Nav from './js/Nav';
 import Main from './js/Main';
 import { IconContext } from 'react-icons';
-function App() {
+const App = () => {
   return (
     <IconContext.Provider
       value={{ color: ' rgba(198, 236, 234, 1)', size: '3rem' }}>
@@ -13,6 +13,6 @@ function App() {
       </div>
     </IconContext.Provider>
   );
-}
+};
 
 export default App;
