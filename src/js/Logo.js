7bfca840e69664b276ccfd6679b51cedc5626d4d
Hezekia Chege Mungai <64218603+Hcmwebs@users.noreply.github.com>
2021-05-01 03:04:39 +1200

    created the logo as a component to be included in the Nav

---
+++ src/js/Logo.js
@@ -0,0 +1,11 @@
+import logo from '../images/logo.png';
+
+const Logo = () => {
+  return (
+    <div className='logo'>
+      <img src={logo} alt='Logo' />
+    </div>
+  );
+};
+
+export default Logo;
