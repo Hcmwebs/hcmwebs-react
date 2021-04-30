fab083c2ebe69fa055d4ee6ab988e6820474eb63
Hezekia Chege Mungai <64218603+Hcmwebs@users.noreply.github.com>
2021-05-01 03:48:13 +1200

    positioned all the nav children

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import { IconContext } from 'react-icons';
 const App = () => {
   return (
     <IconContext.Provider
-      value={{ color: ' rgba(198, 236, 234, 1)', size: '3rem' }}>
+      value={{
+        color: ' rgba(198, 236, 234, 1)',
+        size: '3rem',
+      }}>
       <div className='container'>
         <Nav />
         <Header />
