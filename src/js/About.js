051022169aec59e4e22a0e004a6d653eeecaa3e6
Heze <64218603+Hcmwebs@users.noreply.github.com>
2021-05-05 10:12:20 +1200

    added the profile image and profile text

--- src/js/About.js
+++ src/js/About.js
@@ -1,5 +1,20 @@
 const About = () => {
-  return <section className='about'></section>;
+  const profileImage =
+    'https://res.cloudinary.com/duymkqhnm/image/upload/v1608460164/hcmwebs/hcmwebs/profiles/IMG_3327_l9pp8q.jpg';
+  return (
+    <section className='about'>
+      <div className='about__header'>
+        <img src={profileImage} alt='profileImage' />
+      </div>
+      <div className='about__body'>
+        <p>
+          I am a full-time dad with a passion for coding. I am a self-taught
+          front-end developer. I love designing and building user-friendly
+          interfaces that offer a good user experience.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default About;
