ec940668c66c166fb8da16e70661ff43b7f1b705
Heze <64218603+Hcmwebs@users.noreply.github.com>
2021-05-07 14:39:03 +1200

    added and styled some projects items

--- src/js/Projects.js
+++ src/js/Projects.js
@@ -1,5 +1,29 @@
+import { GiFrenchFries } from 'react-icons/gi';
+import { BsGrid3X2GapFill } from 'react-icons/bs';
 const Projects = () => {
-  return <section className='projects'></section>;
+  return (
+    <section className='projects'>
+      <div>
+        <a
+          href='https://github.com/Hcmwebs/social-proof-section'
+          target='_blank'
+          noreferrer='https://github.com/'>
+          <span>
+            <BsGrid3X2GapFill color='rgba(224, 78, 62, 1)' />
+          </span>
+          Social Proof UI
+        </a>
+      </div>
+      <div>
+        <a href='https://github.com/Hcmwebs/memoryGame'>
+          <span>
+            <GiFrenchFries color='rgba(224, 78, 62, 1)' />
+          </span>
+          Memory game
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
