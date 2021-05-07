fd372dc2f856956b6d98678c16e7b756624b5b33
Heze <64218603+Hcmwebs@users.noreply.github.com>
2021-05-07 18:19:46 +1200

    added different icons for different projects

--- src/js/Projects.js
+++ src/js/Projects.js
@@ -1,11 +1,14 @@
-import { GiFrenchFries } from 'react-icons/gi';
-import { BsGrid3X2GapFill } from 'react-icons/bs';
+import { GiFrenchFries, GiToyMallet, GiShoppingCart } from 'react-icons/gi';
+import { BsGrid3X2GapFill, BsChatQuoteFill } from 'react-icons/bs';
+import { TiWeatherPartlySunny } from 'react-icons/ti';
+import { RiQuestionAnswerFill } from 'react-icons/ri';
+
 const Projects = () => {
   return (
     <section className='projects'>
-      <div>
+      <div classname='projects__item'>
         <a
-          href='https://github.com/Hcmwebs/social-proof-section'
+          href='https://social-proof-section-amber-eta.vercel.app/'
           target='_blank'
           noreferrer='https://github.com/'>
           <span>
@@ -14,12 +17,49 @@ const Projects = () => {
           Social Proof UI
         </a>
       </div>
-      <div>
-        <a href='https://github.com/Hcmwebs/memoryGame'>
+      <div classname='projects__item'>
+        <a href='https://webshop-ui-components.vercel.app/' target='_blank'>
           <span>
-            <GiFrenchFries color='rgba(224, 78, 62, 1)' />
+            <GiShoppingCart color='rgba(224, 78, 62, 1)' />
           </span>
-          Memory game
+          Webshop UI
+        </a>
+      </div>
+
+      <div classname='projects__item'>
+        <a
+          href='https://whackamole-5t4gkvwb2-hcmwebs.vercel.app/'
+          target='_blank'>
+          <span>
+            <GiToyMallet color='rgba(224, 78, 62, 1)' />
+          </span>
+          Whack-a-Darth-Vader
+        </a>
+      </div>
+      <div classname='projects__item'>
+        <a href='https://faq-accordion-card-react.vercel.app/' target='_blank'>
+          <span>
+            <RiQuestionAnswerFill color='rgba(224, 78, 62, 1)' />
+          </span>
+          Frequently Asked
+        </a>
+      </div>
+      <div classname='projects__item'>
+        <a href='https://weather-app-jpdbx25om.vercel.app' target='_blank'>
+          <span>
+            <TiWeatherPartlySunny color='rgba(224, 78, 62, 1)' />
+          </span>
+          Weather app
+        </a>
+      </div>
+      <div classname='projects__item'>
+        <a
+          href='https://coding-bootcamp-testimonials-dun.vercel.app/'
+          target='_blank'>
+          <span>
+            <BsChatQuoteFill color='rgba(224, 78, 62, 1)' />
+          </span>
+          Testimonials
         </a>
       </div>
     </section>
