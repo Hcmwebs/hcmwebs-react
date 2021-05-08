0ccc642071175f3fb8958eacc17c0d58c4ddeb8d
Hezekia Chege Mungai <64218603+Hcmwebs@users.noreply.github.com>
2021-05-08 22:26:03 +1200

    created a Project.js component ot hoist each project
    Signed-off-by: Hezekia Chege Mungai <64218603+Hcmwebs@users.noreply.github.com>

---
+++ src/js/Project.js
@@ -0,0 +1,17 @@
+const Project = ({ BsGrid3X2GapFill }) => {
+  return (
+    <div className='projects__item'>
+      <a
+        href='https://social-proof-section-amber-eta.vercel.app/'
+        target='_blank'
+        noreferrer='https://github.com/'>
+        <span>
+          <BsGrid3X2GapFill color='rgba(224, 78, 62, 1)' />
+        </span>
+        Social Proof UI
+      </a>
+    </div>
+  );
+};
+
+export default Project;
