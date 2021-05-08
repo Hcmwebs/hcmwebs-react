88564ed87eda97e7cf144881bb0f8b54b374100e
Heze <64218603+Hcmwebs@users.noreply.github.com>
2021-05-09 11:29:47 +1200

    created and added the tool component
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

---
+++ src/js/Tool.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Tool = ({ TiHtml5, TiCss3, SiJavascript, SiReact }) => {
+  return (
+    <>
+      <TiHtml5 />
+      <TiCss3 />
+      <SiJavascript />
+      <SiReact />
+    </>
+  );
+};
+
+export default Tool;
