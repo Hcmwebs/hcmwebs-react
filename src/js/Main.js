e9a50a02c516ed742d0a80a677602bcc3da93f8a
Heze <64218603+Hcmwebs@users.noreply.github.com>
2021-05-09 11:27:23 +1200

    added the tools  section
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/js/Main.js
+++ src/js/Main.js
@@ -1,12 +1,14 @@
 import About from './About';
 import Contact from './Contact';
 import Projects from './Projects';
+import Tools from './Tools';
 
 const Main = () => {
   return (
     <main className='main'>
       <About />
       <Projects />
+      <Tools />
       <Contact />
     </main>
   );
