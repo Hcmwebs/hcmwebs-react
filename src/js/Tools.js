bf01e17f742486d5c0c8a814980f8fb876f72660
Heze <64218603+Hcmwebs@users.noreply.github.com>
2021-05-09 11:28:46 +1200

    created and incoporated the tools component
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

---
+++ src/js/Tools.js
@@ -0,0 +1,26 @@
+import { IconContext } from 'react-icons';
+import { TiHtml5, TiCss3 } from 'react-icons/ti';
+import { SiJavascript, SiReact } from 'react-icons/si';
+import Tool from './Tool';
+
+const Tools = tool => {
+  const heading = 'Here are my tools of trade';
+  return (
+    <IconContext.Provider
+      value={{
+        color: ' rgba(224, 78, 62, 1)',
+        size: '10rem',
+      }}>
+      <section className='tools'>
+        <div className='tools__header'>
+          <h4>{heading} </h4>
+        </div>
+        <div className='tools__body'>
+          <Tool TiHtml5={TiHtml5} TiCss3={TiCss3} SiJavascript={SiJavascript} SiReact = {SiReact} />
+        </div>
+      </section>
+    </IconContext.Provider>
+  );
+};
+
+export default Tools;
