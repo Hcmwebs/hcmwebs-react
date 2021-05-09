a42840d048be202d8fd5c157b17ec5f2b5635347
Heze <64218603+Hcmwebs@users.noreply.github.com>
2021-05-09 21:23:51 +1200

    created a container span for the icons
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/js/Tool.js
+++ src/js/Tool.js
@@ -2,12 +2,20 @@ import React from 'react';
 
 const Tool = ({ TiHtml5, TiCss3, SiJavascript, SiReact }) => {
   return (
-    <>
-      <TiHtml5 />
-      <TiCss3 />
-      <SiJavascript />
-      <SiReact />
-    </>
+    <div className='tool'>
+      <span>
+        <TiHtml5 />
+      </span>
+      <span>
+        <TiCss3 />
+      </span>
+      <span>
+        <SiJavascript />
+      </span>
+      <span>
+        <SiReact />
+      </span>
+    </div>
   );
 };
 
