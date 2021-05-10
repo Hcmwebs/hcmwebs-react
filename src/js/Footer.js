63b9110e41e458a53a95f59747830a3cd87673a0
Heze <64218603+Hcmwebs@users.noreply.github.com>
2021-05-10 20:17:47 +1200

    renamed the contact page /section to footer
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/js/Footer.js
+++ src/js/Footer.js
@@ -1,14 +1,23 @@
-import Contacts from './Contacts';
 import { IconContext } from 'react-icons';
+import {
+  FaGithubSquare,
+  FaTwitterSquare,
+  FaEnvelopeSquare,
+} from 'react-icons/fa';
+import Contacts from './Contacts';
 
 const Footer = () => {
   return (
     <IconContext.Provider
       value={{
-        color: 'rgba(224, 78, 62, 1);s',
+        color: 'rgba(224, 78, 62, 1)',
       }}>
       <footer className=' footer'>
-        <Contacts />
+        <Contacts
+          FaGithubSquare={FaGithubSquare}
+          FaTwitterSquare={FaTwitterSquare}
+          FaEnvelopeSquare={FaEnvelopeSquare}
+        />
       </footer>
     </IconContext.Provider>
   );
