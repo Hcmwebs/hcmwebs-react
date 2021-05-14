20655ff3c2c1f2c7881586de49c7d5f6a2a3d522
Heze <64218603+Hcmwebs@users.noreply.github.com>
2021-05-14 22:57:35 +1200

    added the container   main to  the section
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/js/Tools.js
+++ src/js/Tools.js
@@ -9,17 +9,23 @@ const Tools = tool => {
     <IconContext.Provider
       value={{
         color: ' rgba(224, 78, 62, 1)',
-        size: '8rem'
-
+        size: '8rem',
       }}>
-      <section className='tools'>
-        <div className='tools__header'>
-          <h4>{heading} </h4>
-        </div>
-        <div className='tools__body'>
-          <Tool TiHtml5={TiHtml5} TiCss3={TiCss3} SiJavascript={SiJavascript} SiReact = {SiReact} />
-        </div>
-      </section>
+      <main className='main'>
+        <section className='tools'>
+          <div className='tools__header'>
+            <h4>{heading} </h4>
+          </div>
+          <div className='tools__body'>
+            <Tool
+              TiHtml5={TiHtml5}
+              TiCss3={TiCss3}
+              SiJavascript={SiJavascript}
+              SiReact={SiReact}
+            />
+          </div>
+        </section>
+      </main>
     </IconContext.Provider>
   );
 };
