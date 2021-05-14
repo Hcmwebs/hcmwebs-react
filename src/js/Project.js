23b2b720dbde05ede70f03b23b7b5c66d6872615
Heze <64218603+Hcmwebs@users.noreply.github.com>
2021-05-15 08:27:47 +1200

    -added paddingtop and bottom
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/js/Project.js
+++ src/js/Project.js
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 const Project = ({ BsGrid3X2GapFill }) => {
   return (
     <div className='projects__item'>
-      <Link
-        to='https://social-proof-section-amber-eta.vercel.app/'
+      <a
+        href='https://social-proof-section-amber-eta.vercel.app/'
         target='_blank'
         noreferrer>
         <span>
           <BsGrid3X2GapFill />
         </span>
         Social Proof UI
-      </Link>
+      </a>
     </div>
   );
 };
