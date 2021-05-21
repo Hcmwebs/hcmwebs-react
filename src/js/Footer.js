81cae1f72319a09413cc6fb0c212de85401e6798
Heze <64218603+Hcmwebs@users.noreply.github.com>
2021-05-21 12:30:37 +1200

    -modified the footer section with the correct links
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/js/Footer.js
+++ src/js/Footer.js
@@ -1,9 +1,28 @@
-import Contacts from './Contacts';
+import { IconContext } from 'react-icons';
+import ContactLinks from './ContactLinks';
+import {
+  FaGithubSquare,
+  FaTwitterSquare,
+  FaEnvelopeSquare,
+} from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <footer className=' footer'>
-      <Contacts />
+      <IconContext.Provider
+        value={{
+          color: 'rgba(255, 255, 255, 1)',
+          size: '5rem',
+          boxShadow: '0px 10px 15px 2px rgba(0, 0, 0, 0.2)',
+        }}>
+        <div className='footer__links'>
+          <ContactLinks
+            Github={FaGithubSquare}
+            Twitter={FaTwitterSquare}
+            Email={FaEnvelopeSquare}
+          />
+        </div>
+      </IconContext.Provider>
     </footer>
   );
 };
