b05063a28fde475360168dc736d16539bd330ac7
Heze <64218603+Hcmwebs@users.noreply.github.com>
2021-05-21 12:37:05 +1200

    pointed the email to the correct end point with Link

---
+++ src/js/ContactLinks.js
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+
+const ContactLinks = ({ Github, Twitter, Email }) => {
+  return (
+    <>
+      <a href='https://github.com/Hcmwebs' target='_blank' rel='noreferrer'>
+        <Github />
+      </a>
+      <a href='https://twitter.com/hcmwebs' target='_blank' rel='noreferrer'>
+        <Twitter />
+      </a>
+      <Link to='/Contact' target='_blank' rel='noreferrer'>
+        <Email />
+      </Link>
+    </>
+  );
+};
+
+export default ContactLinks;
