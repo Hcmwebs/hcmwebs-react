2f3b489e26834cc7238ad769c8b86e89bdc685a7
Heze <64218603+Hcmwebs@users.noreply.github.com>
2021-05-26 14:29:07 +1200

    corrected a tuypo on the message
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/js/FormSuccess.js
+++ src/js/FormSuccess.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const FormSuccess = () => {
   return (
     <div className='successMessage'>
-      <p>Thank you for contacting us. Someone will be in touch with soon.</p>
+      <p>Thank you for contacting us. Your message has been sent.</p>
       <Link to='/' className='btn'>
         Home
       </Link>
