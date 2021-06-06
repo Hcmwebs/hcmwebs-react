55ba0ed72e34162ab817d5a5a3b309d1f98d3fcc
Heze <64218603+Hcmwebs@users.noreply.github.com>
2021-06-07 10:04:37 +1200

    Set the header section for animations
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/js/Header.js
+++ src/js/Header.js
@@ -6,11 +6,15 @@ const Header = () => {
     <>
       <header className='header'>
         <div className='heroText'>
-          <h1>
-            Hi, I am <span> Heze </span>
-          </h1>
-          <h2>Welcome to my Website</h2>
-          <Link to='/About'>About me</Link>
+          <div className='title'>
+            <h1>Hi, I am Heze.</h1>
+            <p className='first'>I am a Designer</p>
+            <p className='second'>I am a Developer</p>
+          </div>
+          <div className='subtitle'>
+            <h2>Welcome to my Website</h2>
+            <Link to='/About'>About me</Link>
+          </div>
         </div>
       </header>
     </>
