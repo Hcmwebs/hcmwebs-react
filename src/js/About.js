f60c5d4af4822c319bbe2f6de7f7e551696415b8
Hezekia Chege Mungai <64218603+Hcmwebs@users.noreply.github.com>
2021-06-19 01:27:44 +1200

    corrected a typo
    Signed-off-by: Hezekia Chege Mungai <64218603+Hcmwebs@users.noreply.github.com>

--- src/js/About.js
+++ src/js/About.js
@@ -13,7 +13,7 @@ const About = () => {
             I am a self-taught front-end developer and designer. I am a
             full-time dad with a passion for coding. I love designing and
             developing user-friendly interfaces that offer a good user
-            experience. I am you next best friend. Check out some of my many
+            experience. I am your next best friend. Check out some of my many
             project.
           </p>
           <Link to='/projects'>Projects</Link>
